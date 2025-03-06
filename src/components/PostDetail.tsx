eea512b7cffc7497acf7f9bfeefd2ae79f93fe80
Tapan48 <20BDS059@iiitdwd.ac.in>
2025-03-07 03:25:53 +0530

    added comment section feature

--- src/components/PostDetail.tsx
+++ src/components/PostDetail.tsx
@@ -2,6 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import { supabase } from "../supabase-client";
 import { Post } from "./PostList";
 import LikeButton from "./LikeButton";
+import CommentSection from "./CommentSection";
 interface Props {
   postId: number;
 }
@@ -47,6 +48,7 @@ const PostDetail = ({ postId }: Props) => {
     </p>
 
     <LikeButton postId={postId} />
+    <CommentSection postId={postId} />
   </div>
   );
 };
