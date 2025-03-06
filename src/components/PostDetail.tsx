4b131040bbbd28ff08b1d96fccefbb539bca9d4a
Tapan48 <20BDS059@iiitdwd.ac.in>
2025-03-06 14:36:25 +0530

    added tailwind to postdetail component

--- src/components/PostDetail.tsx
+++ src/components/PostDetail.tsx
@@ -27,17 +27,27 @@ const PostDetail = ({ postId }: Props) => {
 
   if (isLoading) return <div>Loading posts...</div>;
   if (error) return <div>Error {error.message}</div>;
-  console.log(data);
-  return <div className="pt-10">
 
+  return (
+    <div className="space-y-6">
+    <h2 className="text-6xl font-bold mb-6 text-center bg-gradient-to-r from-purple-500 to-pink-500 bg-clip-text text-transparent">
+      {data?.title}
+    </h2>
+    {data?.image_url && (
+      <img
+        src={data.image_url}
+        alt={data?.title}
+        className="mt-4 rounded object-cover w-full h-64"
+      />
+    )}
+    <p className="text-gray-400">{data?.content}</p>
+    <p className="text-gray-500 text-sm">
+      Posted on: {new Date(data!.created_at).toLocaleDateString()}
+    </p>
 
-
-    <h2>{data?.title}</h2>
-
-    <img src={data?.image_url} alt={data?.title} />
-    <p>{data?.content}</p>
-    <p>Posted on:{new Date(data!.created_at).toLocaleDateString()}</p>
-  </div>;
+    
+  </div>
+  );
 };
 
 export default PostDetail;
